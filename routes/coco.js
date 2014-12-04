4677f4a9c2d02dc5652b7d05a00b01d39fffc290
Hao8816 <chenhao8816+8816@gmail.com>
2014-12-05 00:45:02 +0800

    fix bug

--- routes/coco.js
+++ routes/coco.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('coco', { title: 'COCO' });
+    res.render('coco.ejs', { title: 'COCO' });
+});
+
+router.get('/chat/', function(req, res) {
+    res.render('coco-chat.ejs', { title: 'COCO Chat room' });
 });
 
 module.exports = router;
