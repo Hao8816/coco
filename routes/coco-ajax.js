aa3d9456b9cd82d1dceee3c5e72f312caa3b5a4f
Hao8816 <chenhao8816+8816@gmail.com>
2015-03-19 21:59:33 +0800

    fix bug

--- routes/coco-ajax.js
+++ routes/coco-ajax.js
@@ -3,23 +3,24 @@ var router = express.Router();
 var blog = require('../coco_core/blog-impl');
 var user = require('../coco_core/user-impl');
 
-router.post('/save_blog', function(req, res) {
+router.post('/save_blog/', function(req, res) {
     blog.saveBlog(req,res);
 });
 
-router.post('/save_topic', function(req, res) {
+router.post('/save_topic/', function(req, res) {
     blog.saveTopic(req,res);
 });
 
-router.get('/get_blog_list', function(req, res) {
+router.post('/get_blog_list/', function(req, res) {
     blog.getBlogList(req,res);
 });
 
-router.get('/get_friend_list', function(req, res) {
+router.get('/get_friend_list/', function(req, res) {
     blog.getBlogList(req,res);
 });
 
-router.get('/get_topic_list', function(req, res) {
+router.post('/get_topic_list/', function(req, res) {
+    console.log(req.param('creator_sha1'))
     blog.getTopicList(req,res);
 });
 
