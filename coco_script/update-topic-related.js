43bb5779e6af41c89e5c5cdac4647e0c4f6a3e5c
Hao8816 <chenhao8816+8816@gmail.com>
2015-04-26 23:41:00 +0800

    fix bug

--- coco_script/update-topic-related.js
+++ coco_script/update-topic-related.js
@@ -2,29 +2,37 @@ var blog_models = require('../coco_core/blog-models');
 var async = require('async');
 
 // 查询数据库所有用户
-blog_models.Blog.all(function(err,result){
+blog_models.Topic.all(function(err,result){
     if(err){
         console.log(err);
     }
     console.log(result[0]);
     result.forEach(function(obj){
-        var blog_sha1 = obj.sha1;
-        var topic_sha1 = obj.topic_sha1;
-        if (topic_sha1 == null){
-            topic_sha1 = '0'
-        }
-        console.log(topic_sha1)
-        // 获取用户的博客的数量
-        blog_models.Topic.find({ sha1: topic_sha1 }).each(function (topic) {
+        var topic_sha1 = obj.sha1
+        async.series({
+            'get_blog_sha1':function(callback){
+                blog_models.Blog.find({topic_sha1:topic_sha1},function(err,blog_list) {
+                    var blog_sha1s = [];
+                    blog_list.forEach(function(object){
+                        blog_sha1s.push(object.sha1)
+                    });
+                    callback(null,blog_sha1s)
+                });
+            }
+        },function(err,data){
+            console.log(data)
+            blog_models.Topic.find({ sha1: topic_sha1 }).each(function (topic) {
+                topic.related = JSON.stringify(data["get_blog_sha1"]);
 
-            var related_sha1s = JSON.parse(topic.related)
-            related_sha1s.push(blog_sha1)
-            topic.related = JSON.stringify(related_sha1s);
-            topic.update_time = obj.time;
-
-        }).save(function (err) {
-            // done!
-            console.log('update topic info success')
+            }).save(function (err) {
+                // done!
+                console.log('update nb_blog success')
+            });
         });
-    });
+
+
+
+    })
+
+
 });
