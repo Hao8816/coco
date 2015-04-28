790cef9e51d4dfafcab59bcf74b6464fc3c8b5da
Hao8816 <chenhao8816+8816@gmail.com>
2015-04-28 08:40:25 +0800

    fix bug

--- coco_core/coco-email.js
+++ coco_core/coco-email.js
@@ -1,15 +1,30 @@
 var nodemailer = require('nodemailer');
 
+
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'sender@gmail.com',
-        pass: 'password'
+    host:"mail.onekoko.com",
+    port:"25"
+    //auth: {
+    //    user: '15238228816@163.com',
+    //    pass: 'chenhao8816'
+    //}
+});
+
+var mailOptions = {
+    from: '15238228816@163.com ', // sender address
+    to: '15238228816@163.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hello world ✔', // plaintext body
+    html: '<b>Hello world ✔</b>' // html body
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
     }
 });
-transporter.sendMail({
-    from: 'sender@address',
-    to: 'receiver@address',
-    subject: 'hello',
-    text: 'hello world!'
-});
+
+
+//module.exports = transporter
