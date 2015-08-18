809a944242a8ac56e97abb7b93bb3bdc31460587
Hao8816 <chenhao8816+8816@gmail.com>
2015-08-18 23:51:33 +0800

    fix bug

--- coco_core/work-models.js
+++ coco_core/work-models.js
@@ -17,7 +17,8 @@ var Works = db.define("works", {
     time          : String,    // 作品的上传时间
     sha1          : String,    // 用户的sha1
     note          : String,    // 作品的描述
-    file_sha1     : String     // 文件的sha1
+    file_sha1     : String,     // 文件的sha1
+    work_id       : String     // 文件的sha1
 }, {
     // with in model method
 });
--- routes/coco.js
+++ routes/coco.js
@@ -13,6 +13,10 @@ router.get('/zss/', function(req, res){
     res.render('coco-zss.ejs', { title: '张闪闪的作品' });
 });
 
+router.get('/zss/detail/', function(req, res){
+    res.render('coco-zss-detail.ejs', { title: '张闪闪的作品详情' });
+});
+
 router.get('/chat/', function(req, res) {
     res.render('coco-chat.ejs', { title: 'COCO Chat' });
 });
